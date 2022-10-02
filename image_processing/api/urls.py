84d42c1bc0cfaa413def17bd192b6611efa7929e
philip-sor <fsorokin746@gmail.com>
2022-10-02 16:07:24 +0300

    added media folder

--- image_processing/api/urls.py
+++ image_processing/api/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('combined_list/', CombinedImageList.as_view(), name='list_combined_img'),
     path('combined_get/<int:from_images>', CombinedImageGet.as_view(), name='get_combined_img')
     # path('combined_add/', CombinedImageCreate.as_view(), name='add_combined')
-
 ]
 
